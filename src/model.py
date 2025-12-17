15a2dad0d4cfa1bdb3cef8d014e72af6a15ba9f1
Kshitij Sharma <143385611+kshitij730@users.noreply.github.com>
2025-12-17 16:03:16 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ def get_model():
     global _model
     if _model is None:
         _model = SentenceTransformer(
-            "sentence-transformers/all-MiniLM-L6-v2",
+            "sentence-transformers/all-MiniLM-L12-v2",
             device="cpu"
         )
     return _model
